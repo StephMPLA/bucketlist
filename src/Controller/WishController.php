589571f4eaf7b3dfe8f97db662dbf3a8b55c35f8
StephMPLA <stephane.mougeot@hotmail.fr>
2024-09-09 20:35:00 +0200

    Formulaire

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\Wish;
+use App\Form\ListType;
 use App\Repository\WishRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use phpDocumentor\Reflection\Types\Object_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -36,7 +39,7 @@ class WishController extends AbstractController
     }*/
 
     //RECUPERE LA BASE DE DONNEE
-    #[Route('/list')]
+    #[Route('/list', name: 'app_wish_list', methods: ['GET'])]
     public function list(WishRepository $wishRepository): Response
     {
         $wish = $wishRepository->findBy(
@@ -44,11 +47,37 @@ class WishController extends AbstractController
             ['dateCreated' => 'DESC'] // Tri : du plus récent au plus ancien
         );
 
-
         return $this->render('wish/list.html.twig',
         ['wish' => $wish]);
     }
-    #[Route('/details/{id}', name: 'details')]
+
+    #[Route('/create', name: 'create', methods: ['GET', 'POST'])]
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $wish = new Wish();
+        $wishForm = $this->createForm(ListType::class, $wish);
+
+        $wishForm->handleRequest($request);
+
+        //Si on envoi le formulaire et si il est valid
+        if ($wishForm->isSubmitted() && $wishForm->isValid()) {
+            //Date de creation ne peut etre nul donc crée la date d'aujourdhui
+            $wish->setDateCreated(new \DateTime());
+            $entityManager->persist($wish);
+            $entityManager->flush();
+
+            //Ajout un message
+            $this->addFlash('success', 'Serie added successfully');
+            //details à besoin d'un id donc lui retourner la valeur
+            //Bonne pratique toujours faire une redirection sur le bouton envoyer, ca evite que
+            // l'utilisateur avec F5 renvoie toujours le meme formulaire plusieurs fois
+            return $this->redirectToRoute('details', ['id' => $wish->getId()]);
+            //Aller dans base.html.twig pour afficher ce message
+        }
+        return $this->render('wish/create.html.twig',['wishCreate' => $wishForm->createView()]);
+    }
+
+    #[Route('/details/{id}', name: 'details',methods: ['GET'])]
     //public function details(int $id, WishRepository $wishRepository): Response
      public function details(Wish $wish): Response
     {
--- src/Entity/Wish.php
+++ src/Entity/Wish.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\WishRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: WishRepository::class)]
 class Wish
@@ -15,16 +16,23 @@ class Wish
     private ?int $id = null;
 
     #[ORM\Column(length: 250)]
+    #[Assert\NotBlank(message: "Le titre ne peut pas être vide.")]
+    #[Assert\Length(
+        max: 250,
+        maxMessage: "Le titre ne peut pas dépasser {{ limit }} caractères."
+    )]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
     private ?string $description = null;
 
     #[ORM\Column(length: 50)]
+    #[Assert\NotBlank(message: "Merci d'indiquer un Auteur")]
+    #[Assert\Length(min:2,max: 50, )]
     private ?string $author = null;
 
     #[ORM\Column]
-    private ?bool $isPublished = null;
+    private ?bool $isPublished = true;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
     private ?\DateTimeInterface $dateCreated = null;
